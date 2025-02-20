5b802c1f6a271ba1dbddb68d23d9b49865d22908
dragon-sys-1 <75516128+dragon-sys-1@users.noreply.github.com>
2025-02-19 21:22:04 -0800

    Update MerkleManager_Test.java

---
+++ src/MerkleManager_Test.java
@@ -0,0 +1,11 @@
+// Group Members:
+// Member 1: Yousif Alrubaye
+// Member 2: Joshua Chambers
+// Member 3: Albeer Malan
+// Member 4: Samim Nasim
+
+public class MerkleManager_Test {
+    public static void main(String[] args) {
+        MerkleManager manager = new MerkleManager();
+        manager.manage();
+    }
